0600059f86a9453cc71e5f6cbae1f1332d13d4cf
bsr <ionson100@gmail.com>
2026-03-30 10:15:14 +0200

    + edit build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -2,11 +2,17 @@
 
 # 1. Настройки (измените под свой проект)
 APP_NAME="observerLm"
-VERSION="1.0.0"
+VERSION="2.0.2"
 DEB_FOLDER="observerlm-deb"
 OUTPUT_DIR="./dist"
 
-echo "--- Начинаем сборку версии $VERSION ---"
+echo "--- Начинаем сборку версии $VERSION ---" 
+
+# Очистка старых результатов сборки
+rm -rf "$OUTPUT_DIR"
+rm -rf "./temp-linux"
+mkdir -p "$OUTPUT_DIR"
+
 mkdir -p $OUTPUT_DIR
 
 # 2. Сборка для Windows (.exe)
@@ -23,13 +29,28 @@ if [ -d "$DEB_FOLDER" ]; then
     dotnet publish -c Release -r linux-x64 --self-contained true -p:PublishSingleFile=true -o "./temp-linux"
     
     # Подготовка структуры deb
-    mkdir -p "$DEB_FOLDER/usr/local/bin"
-    cp "./temp-linux/$APP_NAME" "$DEB_FOLDER/usr/local/bin/"
+    mkdir -p "$DEB_FOLDER/usr/bin"
+    cp "./temp-linux/$APP_NAME" "$DEB_FOLDER/usr/bin/"
+    
+    # Считаем размер всех файлов внутри структуры будущего пакета
+    INSTALLED_SIZE=$(du -sk --exclude=DEBIAN "$DEB_FOLDER" | cut -f1)
+    echo "$INSTALLED_SIZE"
+    
+    # Обновляем поле Installed-Size в файле control
+    sed -i "/^Installed-Size:/" "$DEB_FOLDER/DEBIAN/control"
+    
+    
     
     # Обновляем версию в control (только если работаем в Linux/WSL, где есть sed)
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         sed -i "s/^Version:.*$/Version: $VERSION/" "$DEB_FOLDER/DEBIAN/control"
-        chmod +x "$DEB_FOLDER/usr/local/bin/$APP_NAME"
+        # Если строки Installed-Size нет — добавим, если есть — заменим
+                if grep -q "Installed-Size:" "$DEB_FOLDER/DEBIAN/control"; then
+                    sed -i "s/^Installed-Size:.*$/Installed-Size: $INSTALLED_SIZE/" "$DEB_FOLDER/DEBIAN/control"
+                else
+                    echo "Installed-Size: $INSTALLED_SIZE" >> "$DEB_FOLDER/DEBIAN/control"
+                fi
+        chmod +x "$DEB_FOLDER/usr/bin/$APP_NAME"
         chmod -R 755 "$DEB_FOLDER/DEBIAN/"
         
         echo "--- Упаковка .deb пакета ---"
@@ -40,7 +61,7 @@ if [ -d "$DEB_FOLDER" ]; then
     
     # Очистка временных файлов
     rm -rf "./temp-linux"
-    rm  ./observerlm-deb/usr/local/bin/observerLm
+    rm  ./observerlm-deb/usr/bin/observerLm
 else
     echo "Ошибка: Папка $DEB_FOLDER не найдена!"
 fi
